05975cfa1b167b78e5c132011d610d592ac8dc8f
garciaqueen <mariastetsiuc1234@gmail.com>
2026-02-25 16:45:42 +0100

    Third task.

--- svg/Point.java
+++ svg/Point.java
@@ -2,14 +2,31 @@ public class Point {
     public double x;
     public double y;
 
+
     @Override
     public String toString() {
-        return ("(" + x + "," + y + ")"); 
+        return "Point with x = " + this.x + " y = " + this.y; 
     }
-    public static void init_print(double x, double y) {
-        x = 3.1;
-        y = 5.2;
-        System.out.println(x + " " + y);
+
+    public String toSvg() {
+        return """
+               <svg height="100" width="100" xmlns="http://www.w3.org/2000/svg">\r
+                 <circle r="45" cx=""" //
+         + this.x + " cy=" + this.y + " fill=\"red\" />\r\n" + //
+                        "</svg>";
+    }
+
+    public void tranlate(double dx, double dy) {
+        dx++;
+        dy++;
+    }
+
+    public void translated(double dx, double dy) {
+        Point p2 = new Point();
+        p2.x = 1.0;
+        p2.y = 2.0;
+        p2.x += dx;
+        p2.y += dy;
     }
 
 }
--- svg/main.java
+++ svg/main.java
@@ -1,6 +1,16 @@
 public class Main {
-  static void main() {
-    System.out.println("Hello World!");
+  public static void main(String[] args) {
+    Point p1 = new Point();
+
+    p1.x = 12;
+    p1.y = 22;
+
+    // System.out.println(p1.x);
+    // System.out.println(p1.y);
+
+    // System.out.println(p1.toString());
+    System.out.println(p1.toSvg());
   }
+  
 }
 
