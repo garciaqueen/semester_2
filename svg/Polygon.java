72c1273c96ec4ebfb86f09e22d0d58795f7dd820
maria <135614312+garciaqueen@users.noreply.github.com>
2026-03-04 16:22:02 +0100

    Create Polygon class with SVG generation
    Implement Polygon class to represent a polygon with points and generate SVG representation.

---
+++ svg/Polygon.java
@@ -0,0 +1,36 @@
+public class Polygon {
+
+    private Point[] points;
+
+    public Polygon(Point[] points) {
+        this.points = new Point[points.length];
+
+        for (int i = 0; i < points.length; i++) {
+            this.points[i] = new Point(points[i]);
+        }
+    }
+    @Override
+    public String toString() {
+        StringBuilder st = new StringBuilder("Points array: ");
+        for (Point p: points) {
+            st.append(p.toString()).append("\n");
+        }
+        return st.toString();
+    }
+
+    public String toSvg() {
+        StringBuilder st = new StringBuilder();
+        st.append("""
+                <svg width="300" height="300">
+                  <polygon cx="50" cy="50" r="40" stroke="green" stroke-width="4" fill="yellow""");
+        for (Point p: points) {
+            st.append(p.getX()).append(",").append(p.getY()).append(" ");
+        }
+        st.append("""
+                " style="fill:yellow;stroke:red;stroke-width:2"/>
+                </svg>
+                """);
+
+        return st.toString();
+    }
+}
