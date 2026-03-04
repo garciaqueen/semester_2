0442b34d8e89c1bc93dd391f3712585de3608b43
maria <135614312+garciaqueen@users.noreply.github.com>
2026-03-04 17:33:57 +0100

    Add save method to SvgScene for SVG export
    Implement save method to export SVG representation.

--- svg/SvgScene.java
+++ svg/SvgScene.java
@@ -1,3 +1,6 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 public class SvgScene {
 
     private Polygon[] polygons;
@@ -24,5 +27,35 @@ public class SvgScene {
             }
         }
         return st.toString();
+    }
+
+    public void save(String filePath) {
+        double maxX = 0;
+        double maxY = 0;
+
+        for (Polygon p : polygons) {
+            if (p != null) {
+                BoundingBox box = p.boundingBox();
+                if (box.x() + box.width() > maxX) maxX = box.x() + box.width();
+                if (box.y() + box.height() > maxY) maxY = box.y() + box.height();
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"")
+                .append(maxX)
+                .append("\" height=\"")
+                .append(maxY)
+                .append("\">\n");
+
+        sb.append(this.toSvg());
+
+        sb.append("</svg>");
+
+        try (FileWriter fw = new FileWriter(filePath)) {
+            fw.write(sb.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
