ac13d05f80905cb85124178fe49f50798a4ba2b4
garciaqueen <mariastetsiuc1234@gmail.com>
2026-06-07 19:31:17 +0300

    Deleted files.

--- NonFoodProduct.java
+++
@@ -1,8 +0,0 @@
-import java.util.List;
-
-public class AmbigiousProductException extends RuntimeException {
-    public AmbigiousProductException(List<String> list){
-        super("zdublowane produkty: " + list.toString());
-
-    }
-}
